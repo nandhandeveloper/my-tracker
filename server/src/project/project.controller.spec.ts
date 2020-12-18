9f9c3245ed4904efbb7833e2ffece688c5c4448f
kaushik nandhan <nandhan.developer@gmail.com>
2020-12-18 15:17:27 -0500

    feat(server): es linting issues

--- server/src/project/project.controller.spec.ts
+++ server/src/project/project.controller.spec.ts
@@ -1,11 +1,14 @@
 import { StorySchema } from './../story/story.model';
 import { StoryService } from './../story/story.service';
-import { UnprocessableEntityException, NotFoundException, InternalServerErrorException } from '@nestjs/common';
+import {
+  UnprocessableEntityException,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { ProjectDto } from '../project/dto/project.dto';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectController } from './project.controller';
 import { ProjectService } from './project.service';
-
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProjectSchema, Project } from './project.model';
@@ -19,21 +22,22 @@ describe('ProjectController', () => {
   let projectController: ProjectController;
   // let projectService: ProjectService;
 
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
         rootMongooseTestModule(),
-        MongooseModule.forFeature([{ name: 'Project', schema: ProjectSchema }, { name: 'Story', schema: StorySchema }]),
+        MongooseModule.forFeature([
+          { name: 'Project', schema: ProjectSchema },
+          { name: 'Story', schema: StorySchema },
+        ]),
       ],
       controllers: [ProjectController],
-      providers: [ProjectService, StoryService]
+      providers: [ProjectService, StoryService],
     }).compile();
 
     projectController = module.get<ProjectController>(ProjectController);
     // projectService = module.get<ProjectService>(ProjectService);
   });
-
 
   afterAll(async () => {
     await closeInMongodConnection();
@@ -52,9 +56,11 @@ describe('ProjectController', () => {
     const newProject: ProjectDto = {
       name: 'project1',
       status: ProjectStatus.INACTIVE,
-      isChoosen: false
+      isChoosen: false,
     };
-    const resposne: ProjectDto = await projectController.createProject(newProject);
+    const resposne: ProjectDto = await projectController.createProject(
+      newProject,
+    );
     expect(resposne).toBeDefined();
     expect(resposne).toHaveProperty('_id');
     expect(resposne).toHaveProperty('name', newProject.name);
@@ -62,63 +68,72 @@ describe('ProjectController', () => {
     expect(resposne).toHaveProperty('updatedAt');
   });
 
-
   it('CREATE PROJECT - should throw 422 error when project name already exists in DB', async () => {
     const existingProject: ProjectDto = {
       name: 'project1',
       status: ProjectStatus.ACTIVE,
-      isChoosen: false
+      isChoosen: false,
     };
     const newProject: ProjectDto = {
       name: 'project1',
       status: ProjectStatus.ACTIVE,
-      isChoosen: true
+      isChoosen: true,
     };
 
     await projectController.createProject(existingProject);
-    await expect(projectController.createProject(newProject))
-      .rejects
-      .toThrow(new UnprocessableEntityException('Project with name project1 already exists'));
+    await expect(projectController.createProject(newProject)).rejects.toThrow(
+      new UnprocessableEntityException(
+        'Project with name project1 already exists',
+      ),
+    );
   });
-
-
-
 
   it('FIND BY ID - should return the project details when Id is passed', async () => {
     const incomingProject = {
-      name: "project1",
+      name: 'project1',
       status: ProjectStatus.INACTIVE,
       isChoosen: true,
     };
 
-    const response: ProjectDto = await projectController.createProject(incomingProject);
+    const response: ProjectDto = await projectController.createProject(
+      incomingProject,
+    );
     const id: string = response._id;
 
-    const existingProject: ProjectDto = await projectController.findProjectById(id);
+    const existingProject: ProjectDto = await projectController.findProjectById(
+      id,
+    );
 
     expect(existingProject).toHaveProperty('_id');
     expect(existingProject).toHaveProperty('name', incomingProject.name);
     expect(existingProject).toHaveProperty('status', incomingProject.status);
-    expect(existingProject).toHaveProperty('isChoosen', incomingProject.isChoosen);
+    expect(existingProject).toHaveProperty(
+      'isChoosen',
+      incomingProject.isChoosen,
+    );
     expect(existingProject).toHaveProperty('createdAt');
     expect(existingProject).toHaveProperty('updatedAt');
   });
 
   it('FIND BY ID - should throw not found exception when a project id is not found in DB', async () => {
-    const invalidProjectId: string = '5fb730ca227bd2dcf72acd49';
-    await expect(projectController.findProjectById(invalidProjectId))
-      .rejects
-      .toThrow(new NotFoundException(`No Project found with the id ${invalidProjectId}`));
+    const invalidProjectId = '5fb730ca227bd2dcf72acd49';
+    await expect(
+      projectController.findProjectById(invalidProjectId),
+    ).rejects.toThrow(
+      new NotFoundException(`No Project found with the id ${invalidProjectId}`),
+    );
   });
 
   it('DELETE PROJECT - Should delete the record if present in the database', async () => {
     const incomingProject = {
-      name: "project1",
+      name: 'project1',
       status: ProjectStatus.INACTIVE,
       isChoosen: true,
     };
 
-    const response: ProjectDto = await projectController.createProject(incomingProject);
+    const response: ProjectDto = await projectController.createProject(
+      incomingProject,
+    );
     const id: string = response._id;
 
     const allProjects: ProjectDto[] = await projectController.findAllProject();
@@ -128,44 +143,54 @@ describe('ProjectController', () => {
 
     const projectList: ProjectDto[] = await projectController.findAllProject();
     expect(projectList).toHaveLength(0);
-
   });
 
   it('DELETE PROJECT - should throw error, when an id of INCORRECT format is sent', async () => {
     const dummyId: string = ERROR_PROJECT_ID;
-    await expect(projectController.deleteProject(dummyId))
-      .rejects
-      .toThrow(new InternalServerErrorException('Something went wrong. Please try again'));
+    await expect(projectController.deleteProject(dummyId)).rejects.toThrow(
+      new InternalServerErrorException(
+        'Something went wrong. Please try again',
+      ),
+    );
   });
-
 
   it('DELETE PROJECT - should throw not found exception when a project id is not found in DB', async () => {
     const invalidProjectId: string = INVALID_PROJECT_ID;
 
-    await expect(projectController.deleteProject(invalidProjectId))
-      .rejects
-      .toThrow(new NotFoundException(`No Project found with the id ${invalidProjectId}`));
+    await expect(
+      projectController.deleteProject(invalidProjectId),
+    ).rejects.toThrow(
+      new NotFoundException(`No Project found with the id ${invalidProjectId}`),
+    );
   });
 
   it('UPDATE PROJECT -  should return updated objected', async () => {
     const existingProject = {
-      name: "project1",
+      name: 'project1',
       status: ProjectStatus.INACTIVE,
       isChoosen: true,
     };
 
-    const response: ProjectDto = await projectController.createProject(existingProject);
+    const response: ProjectDto = await projectController.createProject(
+      existingProject,
+    );
     const incomingId = response._id;
     const incomingProject = {
-      name: "project2",
+      name: 'project2',
       status: ProjectStatus.ACTIVE,
       isChoosen: false,
-    }
-    const updatedProject: ProjectDto = await projectController.updateProject(incomingId, incomingProject);
+    };
+    const updatedProject: ProjectDto = await projectController.updateProject(
+      incomingId,
+      incomingProject,
+    );
     expect(updatedProject).toHaveProperty('_id');
     expect(updatedProject).toHaveProperty('name', incomingProject.name);
     expect(updatedProject).toHaveProperty('status', incomingProject.status);
-    expect(updatedProject).toHaveProperty('isChoosen', incomingProject.isChoosen);
+    expect(updatedProject).toHaveProperty(
+      'isChoosen',
+      incomingProject.isChoosen,
+    );
     expect(updatedProject).toHaveProperty('createdAt');
     expect(updatedProject).toHaveProperty('updatedAt');
     expect(updatedProject.createdAt).not.toEqual(updatedProject.updatedAt);
@@ -174,26 +199,30 @@ describe('ProjectController', () => {
   it('UPDATE PROJECT -  should throw not found error when a project with an ID not present in database', async () => {
     const invalidProjectId = INVALID_PROJECT_ID;
     const incomingProject = {
-      name: "project1",
+      name: 'project1',
       status: ProjectStatus.ACTIVE,
       isChoosen: false,
-    }
-    await expect(projectController.updateProject(invalidProjectId, incomingProject))
-      .rejects
-      .toThrow(new NotFoundException(`No Project found with the id ${invalidProjectId}`));
+    };
+    await expect(
+      projectController.updateProject(invalidProjectId, incomingProject),
+    ).rejects.toThrow(
+      new NotFoundException(`No Project found with the id ${invalidProjectId}`),
+    );
   });
 
   it('UPDATE PROJECT -  should throw Something went wrong error when a error projectId is passed as incoming ID', async () => {
     const errorId = ERROR_PROJECT_ID;
     const incomingProject = {
-      name: "project1",
+      name: 'project1',
       status: ProjectStatus.ACTIVE,
       isChoosen: false,
-    }
-    await expect(projectController.updateProject(errorId, incomingProject))
-      .rejects
-      .toThrow(new InternalServerErrorException(`Something went wrong. Please try again`));
+    };
+    await expect(
+      projectController.updateProject(errorId, incomingProject),
+    ).rejects.toThrow(
+      new InternalServerErrorException(
+        `Something went wrong. Please try again`,
+      ),
+    );
   });
-
-
 });
--- server/src/story/story.controller.spec.ts
+++ server/src/story/story.controller.spec.ts
@@ -2,7 +2,7 @@ import { NotFoundException } from '@nestjs/common';
 import { Project } from './../project/project.model';
 import { Test, TestingModule } from '@nestjs/testing';
 import { StoryController } from './story.controller';
-import { MongooseModule } from '@nestjs/mongoose'
+import { MongooseModule } from '@nestjs/mongoose';
 import { closeInMongodConnection, rootMongooseTestModule } from '../testConfig';
 import { ProjectSchema } from '../project/project.model';
 import { StorySchema } from './story.model';
@@ -16,7 +16,6 @@ import { StoryStatus } from '../constants/stroyStatus';
 import { StoryCritical } from '../constants/stroyCritical';
 import { StoryType } from '../constants/stroyType';
 
-
 const INVALID_ID = '5fb730ca227bd2dcf72acd49';
 const ERROR_PROJECT_ID = 'dummyiddummyiddummyid432';
 
@@ -26,26 +25,29 @@ describe('StoryController', () => {
   let projectService: ProjectService;
   let existingProject: ProjectDto;
   let existingStory: StoryDto;
-  let newStory: StoryDto
+  let newStory: StoryDto;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
         rootMongooseTestModule(),
-        MongooseModule.forFeature([{ name: 'Project', schema: ProjectSchema }, { name: 'Story', schema: StorySchema }]),
+        MongooseModule.forFeature([
+          { name: 'Project', schema: ProjectSchema },
+          { name: 'Story', schema: StorySchema },
+        ]),
       ],
       controllers: [StoryController],
-      providers: [StoryService, ProjectService]
+      providers: [StoryService, ProjectService],
     }).compile();
 
-    storyController = module.get<StoryController>(StoryController)
+    storyController = module.get<StoryController>(StoryController);
     storyService = module.get<StoryService>(StoryService);
     projectService = module.get<ProjectService>(ProjectService);
 
     const newProject: ProjectDto = {
       name: 'project1',
       status: ProjectStatus.ACTIVE,
-      isChoosen: true
+      isChoosen: true,
     };
     existingProject = await projectService.createProject(newProject);
 
@@ -58,7 +60,6 @@ describe('StoryController', () => {
       type: StoryType.FEATURE,
     };
     existingStory = await storyService.createStory(newStory);
-
   });
 
   afterEach(async () => {
@@ -80,16 +81,17 @@ describe('StoryController', () => {
     expect(await storyService.findAllStories()).toHaveLength(0);
   });
 
-
   it('DELETE STORY - should throw 404 not found error when given story Id is not present in the database', async () => {
     const storyId = INVALID_ID;
-    await expect(storyController.deleteStory(storyId))
-      .rejects
-      .toThrow(new NotFoundException(`No Story found with the id ${storyId}`));
+    await expect(storyController.deleteStory(storyId)).rejects.toThrow(
+      new NotFoundException(`No Story found with the id ${storyId}`),
+    );
   });
 
   it('FIND ALL STORIES BY PROJECT -  should return a stories based on the project selected', async () => {
-    expect(await storyService.findStoriesByProjectId(existingProject._id)).toHaveLength(1);
+    expect(
+      await storyService.findStoriesByProjectId(existingProject._id),
+    ).toHaveLength(1);
     const storiesList = [
       {
         content: 'some description about the story',
@@ -106,14 +108,16 @@ describe('StoryController', () => {
         critical: StoryCritical.MEDIUM,
         project: existingProject,
         type: StoryType.FEATURE,
-      }
+      },
     ];
     await Promise.all(
       storiesList.map(async (story: StoryDto) => {
         await storyService.createStory(story);
-      })
+      }),
     );
-    expect(await storyController.findStoriesByProjectId(existingProject._id)).toHaveLength(3);
+    expect(
+      await storyController.findStoriesByProjectId(existingProject._id),
+    ).toHaveLength(3);
   });
 
   it('UPDATE STORY -  should update the exiting story', async () => {
@@ -123,7 +127,10 @@ describe('StoryController', () => {
     existingStory.status = StoryStatus.COMPLETE;
     existingStory.project = existingProject;
     existingStory.content = 'edited content for description';
-    const updatedStory: StoryDto = await storyService.updateStory(existingStory._id, existingStory);
+    const updatedStory: StoryDto = await storyService.updateStory(
+      existingStory._id,
+      existingStory,
+    );
     expect(updatedStory).toHaveProperty('_id');
     expect(updatedStory).toHaveProperty('content', existingStory.content);
     expect(updatedStory).toHaveProperty('technology', existingStory.technology);
@@ -138,16 +145,20 @@ describe('StoryController', () => {
 
   it('UPDATE STORY -  should throw 404 error when project with the given id is not found', async () => {
     existingStory.project._id = INVALID_ID;
-    await expect(storyController.updateStory(existingStory._id, existingStory))
-      .rejects
-      .toThrow(new NotFoundException(`No Project found with the id ${INVALID_ID}`));
+    await expect(
+      storyController.updateStory(existingStory._id, existingStory),
+    ).rejects.toThrow(
+      new NotFoundException(`No Project found with the id ${INVALID_ID}`),
+    );
   });
 
   it('UPDATE STORY -  should throw 404 error when story with the given id is not found', async () => {
     existingStory._id = INVALID_ID;
-    await expect(storyController.updateStory(existingStory._id, existingStory))
-      .rejects
-      .toThrow(new NotFoundException(`No Story found with the id ${INVALID_ID}`));
+    await expect(
+      storyController.updateStory(existingStory._id, existingStory),
+    ).rejects.toThrow(
+      new NotFoundException(`No Story found with the id ${INVALID_ID}`),
+    );
   });
 
   it('CREATE STORY - should return all the stories from the database', async () => {
@@ -165,9 +176,8 @@ describe('StoryController', () => {
 
   it('CREATE STORY - should throw NOT FOUND error when project Id is not found', async () => {
     existingStory.project._id = INVALID_ID;
-    await expect(storyController.createStory(existingStory))
-      .rejects
-      .toThrow(new NotFoundException(`No Project found with the id ${INVALID_ID}`));
+    await expect(storyController.createStory(existingStory)).rejects.toThrow(
+      new NotFoundException(`No Project found with the id ${INVALID_ID}`),
+    );
   });
-
 });
